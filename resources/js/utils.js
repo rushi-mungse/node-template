6f47de839b58a2fd08f8bea28bc001177a17fa9a
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-04 16:10:11 +0530

    add: toggle debug btn

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -20,12 +20,33 @@ function notify(msg, status) {
         status === "none"
             ? "white"
             : status === "success"
-            ? "#1fbe1f"
-            : status === "warning"
-            ? "orange"
-            : "#ef3030";
+              ? "#1fbe1f"
+              : status === "warning"
+                ? "orange"
+                : "#ef3030";
     notificationBox.innerHTML = msg;
     setInterval(() => {
         notificationBox.style.display = "none";
     }, 5000);
+}
+
+function toggleDebugBtn() {
+    let debugBtn = document.getElementById("debug");
+    if(debugBtn) {
+        if (!DEBUG_MODE) {
+            debugBtn.classList.remove("flex");
+            debugBtn.classList.add("hidden");
+        } else {
+            debugBtn.classList.remove("hidden");
+            debugBtn.classList.add("flex");
+        }
+    }
+}
+
+function getAction(actionType) {
+    let action = null, actionId = createId();
+    if(actionType === 'IF_CONDITION') 
+        action =  new IfConditionAction(actionId);
+    ACTIONS.set(actionId, action)
+    return action;
 }
