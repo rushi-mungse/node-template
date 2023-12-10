707835a39c67b34a009c3ac5e58eead635da33d5
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2023-12-10 17:19:13 +0530

    add: action for run code

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -46,8 +46,17 @@ function toggleDebugBtn() {
 function getAction(actionType) {
     let action = null,
         actionId = createId();
+    //TODO:
     if (actionType === "IF_CONDITION") action = new IfConditionAction(actionId);
     else if (actionType === "SWITCH") action = new SwitchAction(actionId);
+    else if (actionType === "FOR_LOOP") action = new ForLoopAction(actionId);
+    else if (actionType === "LOOP_DATA") action = new LoopDataAction(actionId);
+    else if (actionType === "CONSOLE_LOG")
+        action = new ConsoleLogAction(actionId);
+    else if (actionType === "NOTIFICATION")
+        action = new NotificationAction(actionId);
+    else if (actionType === "WEBHOOK") action = new WebhookAction(actionId);
+
     ACTIONS.set(actionId, action);
     return action;
 }
